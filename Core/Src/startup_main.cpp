367031230b28f5886b73fee6d34b849881d37a13
tungchiahui <tungchiahui@gmail.com>
2024-08-11 12:09:28 +0800

    Fix DMA

--- Core/Src/startup_main.cpp
+++ Core/Src/startup_main.cpp
@@ -1,10 +1,13 @@
 #include "startup_main.h"
+#include "stm32f1xx_hal_uart.h"
+#include "usart.h"
 
+extern uint8_t rx_buffer[1];
 
 void startup_main(void)
 {
+	HAL_UART_Receive_DMA(&huart1, rx_buffer, 1);
 
-	
 #if isRTOS==0    	//如果是裸机开发
 	for(;;)  //等同于while(true)
 	{
